83e53815af9da4a3466c7379a83dfb7b217cbf68
Aleksandr Ivanov <4lex1v@gmail.com>
2017-07-30 21:14:00 -0400

    Initial commit

---
+++ BoxShooter/Assets/Scripts/BasicMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour {
+
+    public float spinSpeed = 180.0f;
+    public float motionMagnitued = 0.1f;
+
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+    // Update is called once per frame
+    void Update () {
+        
+        if (doSpin) { 
+            // Rotate around the up axis of the gameObject
+            Vector3 angle = Vector3.up * spinSpeed * Time.deltaTime;
+            gameObject.transform.Rotate(angle);
+        }
+
+        if (doMotion) {
+            // move up and down over time
+            Vector3 scale = Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitued;
+            gameObject.transform.Translate(scale);
+        }
+    }
+}
