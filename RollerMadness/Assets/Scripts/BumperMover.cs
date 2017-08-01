81bd4fd02f3d6ee58ccd50ba8b25c624d062a0e5
Aleksandr Ivanov <4lex1v@gmail.com>
2017-08-01 00:24:30 -0400

    [RM] Level 1 & 2

--- RollerMadness/Assets/Scripts/BumperMover.cs
+++ RollerMadness/Assets/Scripts/BumperMover.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class BumperMover : MonoBehaviour {
 
-    public enum MotionDirections { Vertical, Horizontal };
+    public enum MotionDirections { Right, Left };
 
-    public MotionDirections motionType = MotionDirections.Vertical;
+    public MotionDirections motionType = MotionDirections.Right;
+    public bool enableMotion = false;
     
     private float magnitude = 0.2f;
     
     // Update is called once per frame
     void Update () {
-        switch (motionType) {
-            case MotionDirections.Vertical:
-                transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * magnitude);
-                break;
-            case MotionDirections.Horizontal:
-                transform.Translate(Vector3.back * Mathf.Sin(Time.timeSinceLevelLoad) * magnitude);
-                break;
+        if (enableMotion) {
+            switch (motionType) {
+                case MotionDirections.Right:
+                    transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * magnitude);
+                    break;
+                case MotionDirections.Left:
+                    transform.Translate(Vector3.left * Mathf.Sin(Time.timeSinceLevelLoad) * magnitude);
+                    break;
+            }
         }
-        
     }
 }
