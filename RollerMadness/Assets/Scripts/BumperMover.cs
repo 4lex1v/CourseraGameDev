9c90e90a89d14e87f3c35ebb055b2731e4c081d4
Aleksandr Ivanov <4lex1v@gmail.com>
2017-07-31 23:38:33 -0400

    [RM] Bumper movement added, better media and canvas

---
+++ RollerMadness/Assets/Scripts/BumperMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperMover : MonoBehaviour {
+
+    public enum MotionDirections { Vertical, Horizontal };
+
+    public MotionDirections motionType = MotionDirections.Vertical;
+    
+    private float magnitude = 0.2f;
+    
+    // Update is called once per frame
+    void Update () {
+        switch (motionType) {
+            case MotionDirections.Vertical:
+                transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * magnitude);
+                break;
+            case MotionDirections.Horizontal:
+                transform.Translate(Vector3.back * Mathf.Sin(Time.timeSinceLevelLoad) * magnitude);
+                break;
+        }
+        
+    }
+}
