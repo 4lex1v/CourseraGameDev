359c72024f951eb077fd5606cd13b7e31d975878
Aleksandr Ivanov <4lex1v@gmail.com>
2017-08-01 06:46:26 -0400

    [RM] Final improvements

--- RollerMadness/Assets/Scripts/SpawnGameObjects.cs
+++ RollerMadness/Assets/Scripts/SpawnGameObjects.cs
@@ -3,41 +3,44 @@ using System.Collections;
 
 public class SpawnGameObjects : MonoBehaviour {
 
-	public GameObject spawnPrefab;
+    public GameObject spawnPrefab;
 
-	public float minSecondsBetweenSpawning = 3.0f;
-	public float maxSecondsBetweenSpawning = 6.0f;
-	
-	public Transform chaseTarget;
-	
-	private float savedTime;
-	private float secondsBetweenSpawning;
+    public float minSecondsBetweenSpawning = 3.0f;
+    public float maxSecondsBetweenSpawning = 6.0f;
+  
+    public Transform chaseTarget;
+  
+    private float savedTime;
+    private float secondsBetweenSpawning;
+    private GameManager gm;
 
-	// Use this for initialization
-	void Start () {
-		savedTime = Time.time;
-		secondsBetweenSpawning = Random.Range (minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Time.time - savedTime >= secondsBetweenSpawning) // is it time to spawn again?
-		{
-			MakeThingToSpawn();
-			savedTime = Time.time; // store for next spawn
-			secondsBetweenSpawning = Random.Range (minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
-		}	
-	}
+    // Use this for initialization
+    void Start () {
+        gm = GameManager.gm;
+        savedTime = Time.time;
+        secondsBetweenSpawning = Random.Range (minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
+    }
+  
+    // Update is called once per frame
+    void Update () {
+        if (GameManager.gm.gameState == GameManager.gameStates.Playing &&
+            Time.time - savedTime >= secondsBetweenSpawning) // is it time to spawn again?
+        {
+            MakeThingToSpawn();
+            savedTime = Time.time; // store for next spawn
+            secondsBetweenSpawning = Random.Range (minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
+        }	
+    }
 
-	void MakeThingToSpawn()
-	{
-		// create a new gameObject
-		GameObject clone = Instantiate(spawnPrefab, transform.position, transform.rotation) as GameObject;
+    void MakeThingToSpawn()
+    {
+        // create a new gameObject
+        GameObject clone = Instantiate(spawnPrefab, transform.position, transform.rotation) as GameObject;
 
-		// set chaseTarget if specified
-		if ((chaseTarget != null) && (clone.gameObject.GetComponent<Chaser> () != null))
-		{
-			clone.gameObject.GetComponent<Chaser>().SetTarget(chaseTarget);
-		}
-	}
+        // set chaseTarget if specified
+        if ((chaseTarget != null) && (clone.gameObject.GetComponent<Chaser> () != null))
+        {
+            clone.gameObject.GetComponent<Chaser>().SetTarget(chaseTarget);
+        }
+    }
 }
